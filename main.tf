e23fc6787064b309d4f12dbbbb5889c50b122673
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-13 11:31:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "instance_group" {
-  source         = "./modules/instance-group"
+  source         = "./instance-group"
   region         = var.region
   instance_count = var.instance_count
 }
 
 module "load_balancer" {
-  source         = "./modules/load-balancer"
+  source         = "./load-balancer"
   instance_group = module.instance_group.instance_group_name
 }
 
