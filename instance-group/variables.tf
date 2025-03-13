07febde62e77375b37dc63222e37768f752e01a6
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-13 11:25:42 +0530

    Create variables.tf

---
+++ instance-group/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "Region for the instance group"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Initial number of instances"
+  type        = number
+}
